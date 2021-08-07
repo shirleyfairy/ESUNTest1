f74f1af77090ed2375a944a8f9d9370966beaad2
士英 馬 <shiying_ma@hotmail.com>
2021-08-07 22:46:00 +0800

    20210807 ESUNTestDBProject

---
+++ ESUNTestDBProject/ESUNTestDBProject/dbo/Tables/MarriageInfo.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[MarriageInfo] (
+    [statistic_yyy] NVARCHAR (50) NOT NULL,
+    [according]     NVARCHAR (50) NOT NULL,
+    [site_id]       NVARCHAR (50) NOT NULL,
+    [marriage_type] NVARCHAR (50) NOT NULL,
+    [female_age]    NVARCHAR (50) NOT NULL,
+    [male_age]      NVARCHAR (50) NOT NULL,
+    [marry_pair]    NVARCHAR (50) NULL,
+    CONSTRAINT [PK_MarriageInfo] PRIMARY KEY CLUSTERED ([marriage_type] ASC, [statistic_yyy] ASC, [according] ASC, [site_id] ASC, [female_age] ASC, [male_age] ASC)
+);
+
