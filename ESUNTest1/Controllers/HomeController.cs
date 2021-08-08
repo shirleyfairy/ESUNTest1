a4bf677c1d9d6b55b27b0e8fde11515e01f124ca
士英 馬 <shiying_ma@hotmail.com>
2021-08-08 11:46:02 +0800

    20210808

--- ESUNTest1/Controllers/HomeController.cs
+++ ESUNTest1/Controllers/HomeController.cs
@@ -11,6 +11,30 @@ namespace ESUNTest1.Controllers
     {
         public ActionResult Index()
         {
+            APIUtil api = new APIUtil();
+            String apiStr = api.GetGEsunStrData();
+            JsonData apiModel = api.GetEsunModel();
+            RecordsData[] recDatas = apiModel.Result.Records;
+            ESUNTestDB esunDB = new ESUNTestDB();
+            int recordCnt = recDatas.Count();
+            Console.WriteLine(recordCnt);
+            ViewBag.Message += "<tr><th>" + recDatas[0].Year + "</th>"
+                            + "<th>" + recDatas[0].According + "</th>"
+                            + "<th>" + recDatas[0].Site + "</th>"
+                            + "<th>" + recDatas[0].MarriageTp + "</th>"
+                            + "<th>" + recDatas[0].Female + "</th>"
+                            + "<th>" + recDatas[0].Male + "</th>"
+                            + "<th>" + recDatas[0].MarryPair + "</th></tr>";
+            for (int i = 1; i < recDatas.Count(); i++)
+            {
+                ViewBag.Message += "<tr><td>" + recDatas[i].Year + "</td>"
+                            + "<td>" + recDatas[i].According + "</td>"
+                            + "<td>" + recDatas[i].Site + "</td>"
+                            + "<td>" + recDatas[i].MarriageTp + "</td>"
+                            + "<td>" + recDatas[i].Female + "</td>"
+                            + "<td>" + recDatas[i].Male + "</td>"
+                            + "<td>" + recDatas[i].MarryPair + "</td></tr>";
+            }
             return View();
         }
 
